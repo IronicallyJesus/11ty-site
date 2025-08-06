c1fa3e88634d3978bbde5283fa07654691b5201f
giteaadmin <gitea@twk95.net>
2025-08-05 23:10:57 -0400

    implemented blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,18 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/resume");
+  // Add a filter for readable dates using vanilla JS
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    // The toLocaleDateString method can be used to format dates
+    // without any external libraries.
+    // The 'UTC' timeZone option is added to prevent off-by-one day errors.
+    return new Date(dateObj).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      timeZone: 'UTC' 
+    });
+  });
 
   // Add a shortcode for the current year for the footer
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
