4e96a56d49e3315d130cba54bd37999630e93e8f
giteaadmin <gitea@twk95.net>
2025-08-07 13:19:53 -0400

    alert color

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,7 @@ module.exports = function(eleventyConfig) {
   // Shortcode for creating a callout box
   // This is a "paired shortcode" which means it has a start and end tag.
   eleventyConfig.addPairedShortcode("callout", function(content, title) {
-      return `<div class="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
+      return `<div class="bg-slate-800 border-l-4 border-blue-500 text-blue-200 p-4" role="alert">
                   <p class="font-bold">${title}</p>
                   <p>${content}</p>
               </div>`;
