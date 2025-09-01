f1eba628e06c27598b8b7da06929dedfa41de3a0
hermes_bot <hermes_bot@twk95.com>
2025-09-01 17:57:53 -0400

    fix: implement tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,njk,md,js}',
+    './.eleventy.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
