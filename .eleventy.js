b7318b87ba8d189614c4418d13dfcfa64cb3ec9a
hermes_bot <hermes_bot@twk95.com>
2025-09-04 21:26:04 -0400

    fix: fontawesome location

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
   eleventyConfig.addPassthroughCopy({ 'src/sitemap.xml': '/sitemap.xml' });
   eleventyConfig.addPassthroughCopy("src/css/prism-tomorrow.css");
-  eleventyConfig.addPassthroughCopy("src/vendor/fontawesome");
+  eleventyConfig.addPassthroughCopy("src/assets/fontawesome");
 
 
   eleventyConfig.addPlugin(syntaxHighlight);
